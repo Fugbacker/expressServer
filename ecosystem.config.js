41eb17e52ae3ee269380ad018bcdfe4603b481c5
Jhonny Fugbucker <evgenzolotoff@gmail.com>
2025-11-15 14:45:18 +0400

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,17 @@
-export default {
+module.exports = {
   apps: [
     {
-      name: 'expressServer',
-      script: 'npm',
-      args: 'run start',
+      name: 'gcadastr',
+      script: '/var/www/expressServer/server.js', // путь к твоему main server файлу
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
       },
-      error_file: '/var/www/web/log/expressServer/err.log',
-      out_file: '/var/www/web/log/expressServer/out.log',
+      error_file: '/var/www/web/log/gcadastr.su/err.log',
+      out_file: '/var/www/web/log/gcadastr.su/out.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       merge_logs: true,
+      autorestart: true, // чтобы перезапускался при падении
     },
   ],
 };
