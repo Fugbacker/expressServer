a2685a6a9301554464f9985278899911c09401f9
Jhonny Fugbucker <evgenzolotoff@gmail.com>
2025-11-24 15:52:25 +0400

    fix

--- libs/proxy.js
+++ libs/proxy.js
@@ -1,4 +1,6 @@
 export const proxyList = [
   'http://aT22b8hm08:u9qZabSnGd@91.221.70.204:10268',
   'http://MyfsLNNQyC:p0y4ARmPRT@91.221.70.204:9044',
+  'http://fbg1mEe2r7:XkJTEfOua5@91.221.70.204:10309',
+  'http://8b9FoHJ9V2:rzVJJmRo9t@91.221.70.204:13563'
 ];
