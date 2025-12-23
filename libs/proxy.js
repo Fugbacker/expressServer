b9fe443acf80572f75c44273225d2b6d8570332e
Jhonny Fugbucker <evgenzolotoff@gmail.com>
2025-12-23 14:25:59 +0400

    fix

--- libs/proxy.js
+++ libs/proxy.js
@@ -5,7 +5,6 @@ export const proxyList = [
   'http://8b9FoHJ9V2:rzVJJmRo9t@91.221.70.204:13563',
   'http://gxfVMs3F97:caAmvLzF26@91.221.70.204:14725',
   'http://f6RypmOSZy:CPnsMmEnP4@91.221.70.204:14726',
-  'http://9477142421:tnBZdlpSRyOL4yBTAyJA_country-RU_lifetime-1@176.9.113.112:48004', //1GB
-  'http://947714242:hTaKYVjb4cRKuqxn0biJ_country-RU_lifetime-1@176.9.113.112:48004',  //1GB
+  'http://947714242:hTaKYVjb4cRKuqxn0biJ_country-RU@176.9.113.112:48004',  //25GB
 ];
 
